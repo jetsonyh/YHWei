96d8befebd4caafc2faf8e5427bb617ba12b0eaf
yao hao <yh403666@126.com>
2015-06-02 15:19:23 +0800

    1.0
    1.0

---
+++ YHWei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHWei
+//
+//  Created by ly on 15/6/2.
+//  Copyright (c) 2015年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHWei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHWei
+//
+//  Created by ly on 15/6/2.
+//  Copyright (c) 2015年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
